7881507f0ed03bc998d6a66e305f17def317fc08
Khanh Mai <khanh.mai@steinsvik.vn>
2019-08-22 16:12:49 +0700

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+const app = express()
+
+app.use(express.json())
+
+
+let db, trips, expenses
+mongo.connect(
+    url,{ useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        if(err) {
+            console.error(err)
+            return
+        }else{
+            console.log('connected to db')
+        }
+        db = client.db('tripcost')
+        trips = db.collection('trips')
+        expenses = db.collection('expenses');
+    }
+)
+
+app.post('/trip', (req, res) => {
+    const name = req.body.name
+    console.log("name",name);
+    trips.insertOne({name: name}, (err, result) => {
+        if(err){
+            console.error(err)
+            res.status(500).json({err:err})
+            return
+        }
+        console.log(result)
+        res.status(200).json({ok:true})
+    })
+})
+app.get('/trips', (req, res) => { /* */})
+app.post('/expense', (req, res) => { /* */})
+app.get('/expenses', (req, res) => { /* */})
+app.listen(3000, () => console.log('Server ready'))
