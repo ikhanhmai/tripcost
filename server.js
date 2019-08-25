ec47e15d9dbd5d5b0c4111ef56f22f594e1c06da
khanh <ikhanhmai@gmail.com>
2019-08-25 09:21:48 +0700

    complete lession

--- server.js
+++ server.js
@@ -1,41 +1,73 @@
-const express = require('express')
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb://localhost:27017'
-const app = express()
+const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
-let db, trips, expenses
-mongo.connect(
-    url,{ useNewUrlParser: true, useUnifiedTopology: true },
-    (err, client) => {
-        if(err) {
-            console.error(err)
-            return
-        }else{
-            console.log('connected to db')
-        }
-        db = client.db('tripcost')
-        trips = db.collection('trips')
-        expenses = db.collection('expenses');
-    }
-)
+let db, trips, expenses;
+mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    console.log('connected to db');
+  }
+  db = client.db('tripcost');
+  trips = db.collection('trips');
+  expenses = db.collection('expenses');
+});
 
 app.post('/trip', (req, res) => {
-    const name = req.body.name
-    console.log("name",name);
-    trips.insertOne({name: name}, (err, result) => {
-        if(err){
-            console.error(err)
-            res.status(500).json({err:err})
-            return
-        }
-        console.log(result)
-        res.status(200).json({ok:true})
-    })
-})
-app.get('/trips', (req, res) => { /* */})
-app.post('/expense', (req, res) => { /* */})
-app.get('/expenses', (req, res) => { /* */})
-app.listen(3000, () => console.log('Server ready'))
+  const name = req.body.name;
+  console.log('name', name);
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    console.log(result);
+    res.status(200).json({ ok: true });
+  });
+});
+app.get('/trips', (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ trips: items });
+  });
+});
+app.post('/expense', (req, res) => {
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  );
+});
+app.get('/expenses', (req, res) => {
+  expenses.find({ trip: req.body.trip }).toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ expenses: items });
+  });
+});
+app.listen(3000, () => console.log('Server ready'));
